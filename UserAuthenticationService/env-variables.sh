c82a934ed06743965867c7c43f05d1e614195b5e
Imtiaz <imtiazjavatrainer@gmail.com>
2022-09-10 18:53:08 +0530

    fc

---
+++ UserAuthenticationService/env-variables.sh
@@ -0,0 +1,3 @@
+export MYSQL_USERNAME=root
+export MYSQL_PASSWORD=root
+export MYSQL_URL=jdbc:mysql://mysqlservice:3306/registered_users?useSSL=false&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true
---
+++ UserMovieService/env-variables.sh
@@ -0,0 +1,2 @@
+export MONGO_DATABASENAME=movie_db
+export MONGO_URI=mongodb://mongodbservice:27017/movie_db
